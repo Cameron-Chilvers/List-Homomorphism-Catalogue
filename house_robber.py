bc485855e05d869af9f6bfc83b8bdb4a8c33b55b
Cameron-Chilvers-UTS <camchilvers123@gmail.com>
2025-03-18 17:55:34 +1100

    Inital Push Plus house robber leftwards and rightwards

---
+++ house_robber.py
@@ -0,0 +1,75 @@
+def robber_leftwards(arr):
+    print("Leftwards")
+
+    # Creating maximumn list of zero
+    maximums = [0] * len(arr)
+    total_max = 0
+
+    # First value has to be the max
+    maximums[0] = arr[0]
+
+    # Second value max is either the first value or itself
+    maximums[1] = max(arr[0], arr[1])
+
+    # Maximum of the current values is the max of the previous value
+    # Or the maximum two houses ago plus the current value
+    for i in range(2, len(arr)):
+        maximums[i] = max(maximums[i-1], maximums[i-2] + arr[i])
+        
+        # Tracking total max
+        if maximums[i] > total_max:
+            total_max = maximums[i]
+
+    print(maximums)
+
+    return total_max
+
+def robber_rightwards(arr):
+    print("Rightwards")
+
+    # Creating maximumn list of zero
+    maximums = [0] * len(arr)
+    total_max = 0
+
+    # Last value has to be the max
+    maximums[-1] = arr[-1]
+
+    # Second last value max is either the last value or itself
+    maximums[-2] = max(arr[-1], arr[-2])
+
+    # Maximum of the current values is the max of the previous value
+    # Or the maximum two houses forward plus the current value
+    for i in range(len(arr)-3, -1, -1):
+        maximums[i] = max(maximums[i+1], maximums[i+2] + arr[i])
+        
+        # Tracking total max
+        if maximums[i] > total_max:
+            total_max = maximums[i]
+
+    print(maximums)
+
+    return total_max
+
+def robber_homomorphism_worker(arr):
+    pass
+
+def robber_dot_operator(arr1, arr2):
+    pass
+
+if __name__ == '__main__':
+    # test_arr = [3,4,2,1,3,5,1]
+    test_arr = [3,4,2,1,3,5,1,4]
+
+    print(test_arr[:3], test_arr[3:])
+
+    leftwards = robber_leftwards(test_arr)
+    rightwards = robber_rightwards(test_arr)
+
+    homomorphism_left = robber_homomorphism_worker(test_arr[0:3])
+    homomorphism_right = robber_homomorphism_worker(test_arr[3:])
+    homomorphism = robber_dot_operator(homomorphism_left, homomorphism_right)
+
+    print("Result left:", leftwards)
+    print("Result right:", rightwards)
+
+    # print(max(leftwards, rightwards))
