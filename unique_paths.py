1a5141d888262b20096003905d13846b2d27049a
Cameron-Chilvers-UTS <camchilvers123@gmail.com>
2025-04-09 09:44:13 +1000

    feat: Added first draft unique paths dot operator

--- unique_paths.py
+++ unique_paths.py
@@ -1,5 +1,6 @@
 import copy
 from fold_functions import foldl, foldr
+import math
 
 def unique_paths_left(aux_info, val):
     # Using binomal numbers to get value of every point
@@ -29,9 +30,29 @@ def unique_paths_right(val, aux_info):
     pass
 
 def unqiue_paths_dot_operator(arr1, arr2):
-    # Combine using the math
     # Do binomial math here?
-    pass
+
+    # [0] = Combine horizontally
+    # [1] = Combine vertically
+
+    if len(arr1[1]) != len(arr2[1]):
+        print("Cannot compute on different sizes")
+        return []
+
+    # Can change this all out for binomal math, Need to research tho
+    final = []
+    for i in range(len(arr1[1])):
+        print(i, arr1[1][i], arr2[1][-(i + 1)])
+        binom_one = math.comb(arr1[1][i][0], arr1[1][i][1])
+        binom_two = math.comb(arr2[1][-(i + 1)][0], arr2[1][-(i + 1)][1])
+        print(binom_one, binom_two)
+
+        if len(final) == 0:
+            final.append(binom_one * binom_two)
+        else: 
+            final.append(final[-1]+(binom_one * binom_two))
+    
+    return final
 
 
 test_arr = [5, 5]
@@ -44,15 +65,20 @@ half_two  = [split_x, test_arr[1]]
 print('Info: ', half_one, half_two)
 
 aux_info = []
+aux_info_2 = []
 
 
 homomorphism_left = foldl(unique_paths_left, aux_info, half_one)
+print("\n\n")
+homomorphism_left_2 = foldl(unique_paths_left, aux_info_2, half_two)
 
 homomorphism_right = foldr(unique_paths_right, aux_info, half_two)
 
-homomorphism = unqiue_paths_dot_operator(homomorphism_left, homomorphism_right)
+print('\n\n')
+homomorphism = unqiue_paths_dot_operator(homomorphism_left, homomorphism_left_2)
 
 print("Result homomorphism left:", homomorphism_left)
-print("Result homomorphism right:", homomorphism_right)
+print("Result homomorphism left 2:", homomorphism_left_2)
+# print("Result homomorphism right:", homomorphism_right)
 
 print("Final Homomorphism: ", homomorphism)
